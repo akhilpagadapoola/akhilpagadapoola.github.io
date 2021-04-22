c1db67df61180ecc8f3d7767d9cb7c3b088ac752
akhilpagadapoola <akhil.p@volkotech.in>
2021-04-22 09:15:29 +0530

    new update hidden with jquery

--- 50days project/hidden search widget/script.js
+++ 50days project/hidden search widget/script.js
@@ -1,8 +1,31 @@
-const search = document.querySelector('.search')
-const btn = document.querySelector('.btn')
-const input = document.querySelector('.input')
+// const search = document.querySelector('.search')
+// const btn = document.querySelector('.btn')
+// const input = document.querySelector('.input')
 
-btn.addEventListener('click', () => {
-    search.classList.toggle('active')
-    input.focus()
-})
+// btn.addEventListener('click', () => {
+//     search.classList.toggle('active')
+//     input.focus()
+// })
+
+
+
+// $(function() {
+//     $('#open').click(function() {
+//         $('.container').addClass('show-nav');
+      
+//     })
+//     $('#close').click(function() {
+//         $('.container').removeClass('show-nav');
+                
+//     })
+// })
+
+
+// $(".input").focus()  
+
+$(document).ready(function(){    
+    $(".btn").click(function(){    
+        $(".search").toggleClass("active");  
+        $(".input").focus() ;  
+    });    
+});    
