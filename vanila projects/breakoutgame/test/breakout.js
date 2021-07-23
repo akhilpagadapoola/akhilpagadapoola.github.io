e893cfc55f689df777b80adb4cb73c0947617aeb
akhilpagadapoola <akhil.p@volkotech.in>
2021-07-23 20:11:01 +0530

    upd

--- vanila projects/breakoutgame/test/breakout.js
+++
@@ -1,9 +0,0 @@
-const brickInfo = {
-    w: 70,
-    h: 20,
-    padding: 10,
-    offsetX: 45,
-    offsetY: 60,
-    visible: true
-  };
-  module.exports=brickInfo;
--- vanila projects/newyearcountdown/test/newyear.test.js
+++
@@ -1,5 +0,0 @@
-const find= require('./newyear')
-
-test('to find the day',()=>{
-    expect(find(2021)).toEqual(199)
-})
